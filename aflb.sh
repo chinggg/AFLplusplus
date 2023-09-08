4ed05eee10f47a50a6879a968438b4a336da4fcb
chinggg <24590067+chinggg@users.noreply.github.com>
2023-09-08 03:17:37 +0000

    chore: configure OpenAI API with env

--- aflb.sh
+++ aflb.sh
@@ -13,5 +13,10 @@ export CXXFLAGS="-g -fno-inline"
 export AFL_DEBUG=1
 export AFL_USE_ASAN=1
 export AFL_LLVM_INSTRUMENT=classic  # used only when compiling targets, disabled when ./configure
+export OPENAI_API_BASE="https://api.endpoints.anyscale.com/v1"
+export OPENAI_API_MODEL=meta-llama/Llama-2-70b-chat-hf
+# export OPENAI_API_MODEL=codellama/CodeLlama-34b-Instruct-hf
+export OPENAI_API_PROMPT="Give relative complexity score of following function in the form as an integer from 0 to 100. You should answer EXACTLY one integer with NO additional words as they cost me more money.\n"
+export OPENAI_API_MAXLEN=4096
 
 $*
